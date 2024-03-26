075ec4d397c2ba31266a9116f3312c3b0e2d775f
SergioEnriquezMartinez <sergio.enrmar.1@educa.jcyl.es>
2024-03-26 15:58:12 +0100

    Un par de tablas de la bbdd

--- bbdd_personal_tutor_online.sql
+++ bbdd_personal_tutor_online.sql
@@ -1,3 +1,4 @@
+CREATE DATABASE IF NOT EXISTS personal_tutor_online;
 DROP TABLE IF EXISTS alumnos;
 DROP TABLE IF EXISTS profesores;
 DROP TABLE IF EXISTS asignaturas;
@@ -6,59 +7,16 @@ DROP TABLE IF EXISTS chats;
 DROP TABLE IF EXISTS valoraciones;
 
 /*======================== CREAMOS TABLAS ======================================*/
-CREATE TABLE alumnos (
-    id_alumno INT AUTO_INCREMENT PRIMARY KEY,
-    nombre_alumno VARCHAR(50) NOT NULL,
-    apellido1_alumno VARCHAR(50) NOT NULL,
-    apellido2_alumno VARCHAR(50) NULL,
-    email_alumno VARCHAR(255) NOT NULL,
-    password_alumno LONGTEXT NOT NULL,
-    microsof_teams_alumno VARCHAR(255) NOT NULL
-);
+CREATE TABLE Persona {
+    id_persona INT PRIMARY KEY AUTO_INCREMENT,
+    nombre VARCHAR(50) NOT NULL,
+    apellido1 VARCHAR(50) NOT NULL,
+    apellido2 VARCHAR(50) NULL,
+    email VARCHAR(50) NOT NULL,
+    password VARCHAR(50) NOT NULL,
+    usuario_teams VARCHAR(50) NULL
+}
 
-CREATE TABLE profesores (
-    id_profesor INT AUTO_INCREMENT PRIMARY KEY,
-    nombre_profesor VARCHAR(50) NOT NULL,
-    apellido1_profesor VARCHAR(50) NOT NULL,
-    apellido2_profesor VARCHAR(50) NULL,
-    email_profesor VARCHAR(255) NOT NULL,
-    password_profesor LONGTEXT NOT NULL,
-    microsof_teams_profesor VARCHAR(255) NOT NULL,
-    asignatura INT NOT NULL
-);
-
-CREATE TABLE asignaturas (
-    id_asignatura INT AUTO_INCREMENT PRIMARY KEY,
-    nombre_asignatura VARCHAR(50) NOT NULL
-);
-
-CREATE TABLE clases_particulares (
-    id_clase INT AUTO_INCREMENT PRIMARY KEY,
-    id_alumno INT NOT NULL,
-    id_profesor INT NOT NULL,
-    fecha_clase DATETIME NOT NULL
-);
-
-CREATE TABLE chats (
-    id_chat INT AUTO_INCREMENT PRIMARY KEY,
-    id_sala_chat VARCHAR(255) NOT NULL,
-    message LONGTEXT NOT NULL,
-    id_alumno INT NOT NULL,
-    id_profesor INT NOT NULL,
-    fecha_chat DATETIME NOT NULL
-);
-
-CREATE TABLE valoraciones (
-    id_voto INT AUTO_INCREMENT PRIMARY KEY,
-    id_alumno INT NOT NULL,
-    id_profesor INT NOT NULL,
-    valoracion INT(2) NOT NULL
-);
-
-ALTER TABLE profesores ADD FOREIGN KEY (asignatura) REFERENCES asignaturas(id_asignatura);
-ALTER TABLE clases_particulares ADD FOREIGN KEY (id_alumno) REFERENCES alumnos(id_alumno);
-ALTER TABLE clases_particulares ADD FOREIGN KEY (id_profesor) REFERENCES profesores(id_profesor);
-ALTER TABLE chats ADD FOREIGN KEY (id_alumno) REFERENCES alumnos(id_alumno);
-ALTER TABLE chats ADD FOREIGN KEY (id_profesor) REFERENCES profesores(id_profesor);
-ALTER TABLE valoraciones ADD FOREIGN KEY (id_alumno) REFERENCES alumnos(id_alumno);
-ALTER TABLE valoraciones ADD FOREIGN KEY (id_profesor) REFERENCES profesores(id_profesor);
+CREATE TABLE Alumno {
+    
+}
